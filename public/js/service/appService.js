50acca1d534ca4f024652bae6e6e94154c22ffab
Ezequiel Bergamaschi <ebergamaschi@medallia.com>
2015-06-23 00:44:22 -0300

    removing service file not detected by heroku

--- public/js/service/appService.js
+++ public/js/service/appService.js
@@ -4,4 +4,11 @@ angular.module("fifa").service("tournamentService", ["$http", function($http) {
             return data;
         });
     }
-}]);
+}])
+    .service("matchService", ["$http", function($http) {
+        this.getMatches = function() {
+            return $http.get("/api/match").then(function(data) {
+                return data;
+            });
+        }
+    }]);
