4cb81b2b49e280224361e4d79c0306e0fad752a6
Ezequiel Bergamaschi <ebergamaschi@medallia.com>
2015-09-20 02:37:36 -0300

    constants refactor

--- app/constants.js
+++ app/constants.js
@@ -1,18 +1,8 @@
-function define(name, value) {
-    Object.defineProperty(exports, name, {
-        value:      value,
-        enumerable: true
-    });
-}
-// MONGO
-define("MONGO_ID", "_id");
-// Team type
-define("HOME", "home");
-define("AWAY", "away");
-
-// Player type
-define("PLAYER", "player");
-define("PARTNER", "partner");
-
-// data
-define("GOALS", "goals");
+module.exports = {
+    "MONGO_ID": "_id",
+    "HOME": "home",
+    "AWAY": "away",
+    "PLAYER": "player",
+    "PARTNER": "partner",
+    "GOALS": "goals"
+};
